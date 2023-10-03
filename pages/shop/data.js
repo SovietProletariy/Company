886f36f59787422b909a51498da5eeba90f2bc4b
SovietProletariy <petrenkob444@gmail.com>
2023-10-03 22:19:53 +0300

    DDD

--- pages/shop/data.js
+++ pages/shop/data.js
@@ -1,7 +1,7 @@
 export const states = {
     array : [
-        {id:1,text:"CRM",path:1,},
-        {id:1,text:"CRM",path:1},
-        {id:1,text:"CRM",path:1}
+        {id:1,text:"Приготовленная курица / 2 шт. / 4 Р.Д.",path:1,},
+        {id:1,text:"Йогурт / 20 шт. / 2 Р.Д.",path:1},
+        {id:1,text:"Кожанная одежда / 1 сэт / 8 Р.Д.",path:1}
     ],
 };
